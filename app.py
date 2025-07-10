023e360c61d76152a623ce5c7085cfa4103b4e2c
tomisaurio <tomisaurio09@gmail.com>
2025-07-10 20:13:19 +0000

    Dia 4 terminado, mysql funciona y ya verifique las rutas con postman

--- app.py
+++ app.py
@@ -9,12 +9,16 @@ def create_app():
     load_dotenv()  # <-- Asegura que se cargue el .env
 
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
-    app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")
+    #app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
+    #app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")
     app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(minutes=30)
     app.config["JWT_REFRESH_TOKEN_EXPIRES"] = timedelta(days=7)
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("SQLALCHEMY_DATABASE_URI")
+    #app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("SQLALCHEMY_DATABASE_URI")
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:KOBQSTPpjMGwNnTWboJGBSeIJYLHrFnk@trolley.proxy.rlwy.net:33788/railway"
+    app.config['SECRET_KEY'] = 'una_clave_segura'
+    app.config['JWT_SECRET_KEY'] = 'clave_super_secreta'
 
     jwt = JWTManager(app)
     db.init_app(app)
--- models.py
+++ models.py
@@ -5,7 +5,7 @@ class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
-    password =db.Column(db.String(100), nullable=False)
+    password =db.Column(db.String(1024), nullable=False)
 
 class Question(db.Model):
     id = db.Column(db.Integer, primary_key=True)
