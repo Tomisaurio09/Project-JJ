51ea94f48320707cc330b4d45b4352fe309b7b97
tomisaurio <tomisaurio09@gmail.com>
2025-07-10 19:40:19 +0000

    cambios en app y env

--- app.py
+++ app.py
@@ -2,26 +2,25 @@ from flask import Flask
 from extensions import db, migrate
 from flask_jwt_extended import JWTManager
 from datetime import timedelta
-import os
 from dotenv import load_dotenv
-
-load_dotenv()
+import os
 
 def create_app():
+    load_dotenv()  # <-- Asegura que se cargue el .env
+
     app = Flask(__name__)
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
-
-    app.config["JWT_SECRET_KEY"] = os.environ.get("JWT_SECRET_KEY")
+    app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
+    app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")
     app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(minutes=30)
     app.config["JWT_REFRESH_TOKEN_EXPIRES"] = timedelta(days=7)
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("SQLALCHEMY_DATABASE_URI")
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+    jwt = JWTManager(app)
     db.init_app(app)
     migrate.init_app(app, db)
-    JWTManager(app)
 
     from routes import blueprint
     app.register_blueprint(blueprint, url_prefix='/api')
-
+    
     return app
