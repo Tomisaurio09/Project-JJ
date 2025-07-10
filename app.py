9f35c9e50f5e8c47e67dc6b6440fabe22bf3acad
tomisaurio <tomisaurio09@gmail.com>
2025-07-10 18:29:48 +0000

    añadi nuevos archivos para el riley

--- app.py
+++ app.py
@@ -2,13 +2,19 @@ from flask import Flask
 from extensions import db, migrate
 from flask_jwt_extended import JWTManager
 from datetime import timedelta
+import os
+from dotenv import load_dotenv
+
+
+
 
 def create_app():
+    load_dotenv()
     app = Flask('__name__')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SECRET_KEY'] = 'thisisthesecretkey'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-    app.config["JWT_SECRET_KEY"] = "clave_super_secreta"
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+    app.config["JWT_SECRET_KEY"] = os.environ.get("JWT_SECRET_KEY")
     app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(minutes=30)
     app.config["JWT_REFRESH_TOKEN_EXPIRES"] = timedelta(days=7)
     jwt = JWTManager(app)
