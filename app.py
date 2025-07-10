6cfc986466cf1d1c387f317dccee0439d0e5ecdf
tomisaurio <tomisaurio09@gmail.com>
2025-07-10 18:55:34 +0000

    cambios en app.py para implementar el load_dotenv

--- app.py
+++ app.py
@@ -5,28 +5,23 @@ from datetime import timedelta
 import os
 from dotenv import load_dotenv
 
-
-
+load_dotenv()
 
 def create_app():
-    load_dotenv()
-    app = Flask('__name__')
+    app = Flask(__name__)
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SECRET_KEY'] = 'thisisthesecretkey'
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+
     app.config["JWT_SECRET_KEY"] = os.environ.get("JWT_SECRET_KEY")
     app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(minutes=30)
     app.config["JWT_REFRESH_TOKEN_EXPIRES"] = timedelta(days=7)
-    jwt = JWTManager(app)
 
     db.init_app(app)
     migrate.init_app(app, db)
+    JWTManager(app)
 
     from routes import blueprint
     app.register_blueprint(blueprint, url_prefix='/api')
-    
+
     return app
-
-
-
-
