7a86db5f2916d152472ed5e8eaa009b1fd98eb23
tomisaurio <tomisaurio09@gmail.com>
2025-07-14 17:55:26 +0000

    change in datetime

--- models/note.py
+++ models/note.py
@@ -1,9 +1,10 @@
 from app import db
 import datetime
+from datetime import datetime, timezone
 
 class Notes(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     title  = db.Column(db.String(100), nullable=False)
     content = db.Column(db.Text, nullable=False)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    created_at = db.Column(db.DateTime, default=datetime.now(timezone.utc))
