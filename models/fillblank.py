0d77c93e5d31710cc50b06d022940194803c7bb5
tomisaurio <tomisaurio09@gmail.com>
2025-07-15 02:31:52 +0000

    Ya deberia estar todo bien

--- models/fillblank.py
+++ models/fillblank.py
@@ -2,6 +2,6 @@ from app import db
 
 class FillBlank(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     sentence = db.Column(db.String(255), nullable=False)
     hidden_word = db.Column(db.String(100), nullable=False)
